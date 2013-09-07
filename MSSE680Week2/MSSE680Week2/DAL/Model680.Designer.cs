fae688c107f805e09d3d135d33dfd4c769cab4e5
nolsen7187 <nolsen@regis.edu>
2013-09-06 22:19:36 -0600

    test

---
+++ MSSE680Week2/MSSE680Week2/DAL/Model680.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\NOLSEN\Documents\GitHub\MSSE680\Week2\MSSE680Week2\MSSE680Week2\DAL\Model680.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
